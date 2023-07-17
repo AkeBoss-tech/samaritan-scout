f38be8b52cadc692ccb634e40026188c5f177e20
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2023-07-17 13:19:41 -0400

    revert changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import NotFound from './pages/NotFound';
 
-const App: React.FC = () => {
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/samaritan-scout/" element={<Home />} />
-        <Route path="/samaritan-scout/about" element={<About />} />
-        <Route path="/samaritan-scout/*" element={<NotFound />} /> 
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 };
 
