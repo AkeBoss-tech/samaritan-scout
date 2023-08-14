3b7878f7eeb754651f4a2e97dbf77eb52016eaff
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2023-08-14 18:14:24 -0400

    update home to make it like option 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -133,7 +133,7 @@ const ReactHome = () => {
               we can help nonprofit organizations we care about
             </Typography>
           </Grid>
-          <Grid xs={10} sm={7} md={7} lg={7} xl={7} textAlign={"center"}>
+          <Grid xs={10} sm={6} md={5} lg={4} xl={4} textAlign={"center"}>
             <TextField
               variant="outlined"
               placeholder="Enter keywords..."
@@ -143,18 +143,7 @@ const ReactHome = () => {
               sx={{ backgroundColor: "#fff", input: { color: "black" } }}
             />
           </Grid>
-          <Grid xs={10} sm={2} md={2} lg={2} xl={2} textAlign={"center"} alignItems={"center"} sx={{ paddingTop: "3px" }}>
-            <Button
-              variant="contained"
-              color="primary"
-              id="inPerson"
-              fullWidth={true}
-              sx={{ height: 55 }}
-            >
-              <SearchIcon />
-            </Button>
-          </Grid>
-          <Grid xs={10} sm={4} md={4} lg={4} xl={4} textAlign={"center"}
+          <Grid xs={10} sm={3} md={3} lg={3} xl={3} textAlign={"center"}
             alignItems={"center"}
             justifyContent="center">
             <TextField
@@ -167,15 +156,23 @@ const ReactHome = () => {
             />
             
           </Grid>
-          <Grid xs={10} sm={4} md={3} lg={3} xl={3} textAlign={"left"} alignItems={"center"} sx={{ paddingTop: "3px" }}>
-            <ButtonGroup fullWidth={true}>
-              <Button variant="contained" color="primary" id="inPerson" sx={{ height: 55 }}>
-                In-Person
-              </Button>
-              <Button variant="contained" color="primary" id="remote" sx={{ height: 55 }}>
-                Remote
-              </Button>
-            </ButtonGroup>
+          <Grid xs={10} sm={2} md={2} lg={2} xl={2} textAlign={"center"} alignItems={"center"} sx={{ paddingTop: "3px" }}>
+            <Button
+              variant="contained"
+              color="primary"
+              id="inPerson"
+              fullWidth={true}
+              sx={{ height: 55 }}
+            >
+              <SearchIcon />
+            </Button>
+          </Grid>
+          
+          <Grid xs={11} textAlign={"center"} alignItems={"center"} sx={{ paddingTop: "3px" }}>
+          
+            <FormControlLabel control={<Checkbox defaultChecked sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }} />} label="In Person" sx={{ fontSize: "24px" }}/>
+            <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }} />} label="Remote" sx={{ fontSize: "24px" }}/>
+          
           </Grid>
           <Grid xs={12} textAlign={"center"}>
             <Box sx={{ height: 35 }}></Box>
